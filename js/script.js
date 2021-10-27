121bffdc0a06e3e579e6e293afac14e6ec15a980
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-27 19:34:31 +0200

    aggiunto pulsante illumina

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 //Toggle darkMode
 
 const darkModeBtn = document.querySelector(".toggleDarkMode");
+const darkModeLogop = document.querySelector(".positive");
+const darkModeLogon = document.querySelector(".negative");
+const darkModeComp = document.querySelector(".competenze");
+const darkModeHob = document.querySelector(".hobbies");
+
+
+
 darkModeBtn.addEventListener(
   "click",
   () => {
     document.body.classList.toggle("darkMode");
-    
+    darkModeLogop.classList.toggle("hide");
+    darkModeLogon.classList.toggle("show");
+    darkModeComp.classList.toggle("darkestMode");
+    darkModeHob.classList.toggle("darkestMode");
+
   }
+);
+
+const lightBtn = document.querySelector(".lightsOn");
+
+lightBtn.addEventListener(
+    "click",
+    () => {
+    darkModeHob.classList.toggle("light");    
+    }
 );
 
